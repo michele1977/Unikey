9f4eb176e0c3c151788166daec62fc7aa191b6a6
Mallqui <hendrichmall99@gmail.com>
2019-12-30 15:56:10 +0100

    Hendrich - creato UserRepository, UserService, UserController e aggiornato Bootstrap

---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnikeyFactoryTest.Context;
+
+namespace UnikeyFactoryTest.Repository
+{
+    public class UserRepository
+    {
+        TestPlatformDBEntities _context = new TestPlatformDBEntities();
+        public bool FindUser(User user)
+        {
+            return _context.Users.ToList().Exists(x => x.Username == user.Username && x.Password == user.Password);
+        }
+    }
+}
---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Service/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnikeyFactoryTest.Context;
+using UnikeyFactoryTest.Repository;
+
+namespace UnikeyFactoryTest.Service
+{
+    public class UserService
+    {
+        UserRepository repository = new UserRepository();
+
+        public bool IsUser(User user)
+        {
+            return repository.FindUser(user);
+        }
+    }
+}
