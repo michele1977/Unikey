9e02d975b907591f5b32f72110d00879358db6aa
CamilloPositano <xcodicsx@gmail.com>
2020-01-02 16:35:17 +0100

    Camillo - Aggiunta del Domain

---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Domain/AdministratedQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnikeyFactoryTest.Domain
+{
+    public class AdministratedQuestion
+    {
+        public AdministratedQuestion()
+        {
+            this.AdministratedAnswers = new HashSet<AdministratedAnswer>();
+        }
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int AdministratedTestId { get; set; }
+        public virtual ICollection<AdministratedAnswer> AdministratedAnswers { get; set; }
+        public virtual AdministratedTest AdministratedTest { get; set; }
+    }
+}
