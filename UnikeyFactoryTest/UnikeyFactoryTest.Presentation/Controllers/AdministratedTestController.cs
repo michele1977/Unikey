5113860786291e72c9584623838a8f7174adb0fa
CamilloPositano <xcodicsx@gmail.com>
2020-01-03 16:13:07 +0100

    Camillo - View e nuovo boostrap

---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/AdministratedTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnikeyFactoryTest.Domain;
+using UnikeyFactoryTest.Presentation.Models;
+using UnikeyFactoryTest.Service;
+
+namespace UnikeyFactoryTest.Presentation.Controllers
+{
+    public class AdministratedTestController : Controller
+    {
+        private AdministratedTestService service;
+        // GET: AdministratedTest
+        public ActionResult TestStart()
+        {
+            var model = new AdministratedTestModel();
+            return View("TestStart", model);
+        }
+        [HttpPost]
+        public ActionResult BeginTest(AdministratedTestModel model)
+        {
+            var subject = model.Name + " " + model.Surname;
+            //creo un test temporaneo da sostire con quello repertio dalla URL
+            var test = new Test();
+            model.Test = service.AdministratedTest_Builder(test, subject);
+            return View("Test", model);
+        }
+    }
+}
---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Models/AdministratedTestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UnikeyFactoryTest.Domain;
+
+namespace UnikeyFactoryTest.Presentation.Models
+{
+    public class AdministratedTestModel
+    {
+        public AdministratedTest Test { get; set; }
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
