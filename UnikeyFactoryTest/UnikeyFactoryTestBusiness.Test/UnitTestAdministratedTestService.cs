0e1d50fb8c1588e9b05a8b551c70d7098f450a22
Daniele <daniele.tulli93@gmail.com>
2020-01-04 16:48:04 +0100

    Daniele - Aggiunta UnitTest

---
+++ UnikeyFactoryTest/UnikeyFactoryTestBusiness.Test/UnitTestAdministratedTestService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnikeyFactoryTestBusiness.Test
+{
+    [TestClass]
+    public class UnitTestAdministratedTestService
+    {
+        [TestMethod]
+        public void AdministratedTest_Builder_OK()
+        {
+        }
+
+        [TestMethod]
+        public void Add_OK()
+        {
+        }
+
+        [TestMethod]
+        public void Update_Save_OK()
+        {
+        }
+
+        [TestMethod]
+        public void GetAdministratedTestById_OK()
+        {
+        }
+    }
+}
