2c85c2b343eeb1b3ce0ca7cbaadca8162c9b86f0
CamilloPositano <xcodicsx@gmail.com>
2020-01-04 10:33:08 +0100

    Camillo -Prima parte per salvare l'administrated test

--- UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/AdministratedTestController.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/AdministratedTestController.cs
@@ -12,6 +12,7 @@ namespace UnikeyFactoryTest.Presentation.Controllers
     public class AdministratedTestController : Controller
     {
         private AdministratedTestService service = new AdministratedTestService();
+        private TestService testService = new TestService();
         // GET: AdministratedTest
         
         public ActionResult TestStart()
@@ -25,6 +26,7 @@ namespace UnikeyFactoryTest.Presentation.Controllers
         {
             var subject = model.Name + " " + model.Surname;
             //creo un test temporaneo da sostire con quello repertio dalla URL
+            //var test = testService.GetTestById(model.URL);
             var test = new Test();
             model.Test = service.AdministratedTest_Builder(test, subject);
             return View("Test", model);
@@ -32,10 +34,26 @@ namespace UnikeyFactoryTest.Presentation.Controllers
 
         public ActionResult SaveTest(AdministratedTestModel model, FormCollection form)
         {
+            //popolo il dictionary con domanda e relativa risposta
             foreach (var key in form.AllKeys)
             {
                 model.QuestionAnswerDictionary[int.Parse(key)] = int.Parse(form[key]);
             }
+
+            //var test = testService.GetTestById(model.URL);
+            //var testQuestions = service.AdministratedTest_Builder(model.URL).Questions;
+            
+
+            foreach (var question in model.QuestionAnswerDictionary)
+            {
+                //var specificQuestion = testQuestions.SingleOrDefault(x => x.Id == question.Key);
+                //if (specificQuestion != null)
+                //{
+                //    var theAnswer = specificQuestion.Answers.SingleOrDefault(x => x.Id == question.Value);
+                //    theAnswer.
+                //}
+            }
+            
             return View("TestStart");
         }
     }
