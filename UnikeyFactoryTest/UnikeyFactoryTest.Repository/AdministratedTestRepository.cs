d6a7328fba5c5e0de8bec70744c89886cf78733f
CamilloPositano <xcodicsx@gmail.com>
2020-01-06 12:18:53 +0100

    modifica Update Save repository

--- UnikeyFactoryTest/UnikeyFactoryTest.Repository/AdministratedTestRepository.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Repository/AdministratedTestRepository.cs
@@ -68,6 +68,10 @@ namespace UnikeyFactoryTest.Repository
 
                 _ctx.AdministratedTests.Attach(upTestDB);
                 _ctx.Entry(upTestDB).State = System.Data.Entity.EntityState.Modified;
+
+                var AdminisratedTest = _ctx.AdministratedTests.FirstOrDefault(t => t.Id == adTest.Id);
+
+                
                 
                 //_ctx.AdministratedTests.AddOrUpdate(upTestDB);
                 _ctx.SaveChanges();
