a2b35af68b119ebde282d6fef53f27d5f48a5130
Daniele <daniele.tulli93@gmail.com>
2020-01-17 15:15:20 +0100

    Daniele - fix

---
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/App_Start/MapConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using AutoMapper;
+using UnikeyFactoryTest.Context;
+using UnikeyFactoryTest.Domain;
+
+namespace UnikeyFactoryTest.Presentation
+{
+    public class MapConfig
+    {
+        public static void RegisterMap()
+        {
+            var userConfig = new MapperConfiguration(cfg => cfg.CreateMap<User, UserBusiness>()
+                .ForMember(ub => ub.Tests, u => u.Ignore())
+            );
+
+            var testConfig = new MapperConfiguration(cfg => cfg.CreateMap<Test, TestBusiness>()
+                .ForMember(tb => tb.Questions, t => t.Ignore())
+                .ForMember(tb => tb.AdministratedTests, t => t.Ignore())
+            );
+
+            var questionConfig = new MapperConfiguration(cfg => cfg.CreateMap<Question, QuestionBusiness>()
+                .ForMember(qb => qb.Answers, q => q.Ignore())
+            );
+
+            var answerConfig = new MapperConfiguration(cfg => cfg.CreateMap<Answer, AnswerBusiness>());
+
+            var adTestConfig = new MapperConfiguration(cfg => cfg.CreateMap<AdministratedTest, AdministratedTestBusiness>()
+                .ForMember(atb => atb.AdministratedQuestions, at => at.Ignore())
+            );
+
+            var adQuestConfig = new MapperConfiguration(cfg => cfg.CreateMap<AdministratedQuestion, AdministratedQuestionBusiness>()
+                .ForMember(aqb => aqb.AdministratedAnswers, aq => aq.Ignore())
+            );
+
+            var adAnsConfig = new MapperConfiguration(cfg => cfg.CreateMap<AdministratedAnswer, AdministratedAnswerBusiness>()
+                .ForMember(aab => aab.AdministratedQuestion, aa => aa.Ignore())
+            );
+        }
+    }
+}
