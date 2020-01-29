57ee00d35aaa2d996cfc406a9870a09306962422
andrea-77 <59687509+andrea-77@users.noreply.github.com>
2020-01-29 10:05:25 +0100

    no message

---
+++ UnikeyFactoryTest/UnikeyFactoryTest_WebApi/Controllers/ExTestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UnikeyFactoryTest_WebApi.Controllers
+{
+    public class ExTestController : ApiController
+    {
+        // GET: api/ExTest
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ExTest/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ExTest
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ExTest/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ExTest/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
