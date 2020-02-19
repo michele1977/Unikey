495b8890340104f1465e92844383d5f0adec5fd3
Abominion <renzolog@hotmail.it>
2020-02-19 13:09:03 +0100

    Added TextSearch_Ok UnitTest Method

---
+++ UnikeyFactoryTest/UserTest/UnikeyFactoryTest.TextSearch.UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnikeyFactoryTest.Context;
+
+namespace UserTest
+{
+    [TestClass]
+    public class UnikeyFactoryTest
+    {
+        [TestMethod]
+        public void TextSearch_Ok()
+        {
+            string testName = "RandomTitleToMakeThisTestRunOk";
+            using (TestPlatformDBEntities ctx = new TestPlatformDBEntities())
+            {
+                using (DbContextTransaction transaction = ctx.Database.BeginTransaction())
+                {
+                    Test test = new Test()
+                    {
+                        AdministratedTests = new List<AdministratedTest>(),
+                        Date = DateTime.Now,
+                        Questions = new List<Question>(),
+                        Title = testName,
+                        URL = "randomUrl",
+                        User = null,
+                        UserId = 0
+                    }
+
+                    transaction.Rollback();
+                }
+            }
+        }
+    }
+}
