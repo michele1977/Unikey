57ec16d0edf45ac87e66d2a6cec371f406529856
andrea-77 <59687509+andrea-77@users.noreply.github.com>
2020-02-19 15:33:10 +0100

    no message

---
+++ UnikeyFactoryTest/UserTest/UnikeyFactoryTest_AdministratedTestService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnikeyFactoryTest.Service;
+
+namespace UserTest
+{
+    [TestClass]
+    public class UnikeyFactoryTest_AdministratedTestService
+    {
+        [TestMethod]
+        public async Task AdministratedTestService_Next_OK()
+        {
+            AdministratedTestService service = new AdministratedTestService();
+            var actualTest = await service.GetAdministratedTestById(1349);
+            var firstQuestion = actualTest.AdministratedQuestions[0];
+            var nextQuestion = await service.Next(actualTest, firstQuestion.Position + 1);
+
+            Assert.AreEqual(actualTest.AdministratedQuestions[1], nextQuestion);
+        }
+
+        [TestMethod]
+        public async Task AdministratedTestService_Previous_OK()
+        {
+            AdministratedTestService service = new AdministratedTestService();
+            var actualTest = await service.GetAdministratedTestById(1349);
+            var secondQuestion = actualTest.AdministratedQuestions[1];
+            var previousQuestion = await service.Previous(actualTest, secondQuestion.Position - 1);
+
+            Assert.AreEqual(actualTest.AdministratedQuestions[0], previousQuestion);
+        }
+    }
+}
--- UnikeyFactoryTest/UserTest/UnikeyFactoryTest_Service_Test.cs
+++ UnikeyFactoryTest/UserTest/UnikeyFactoryTest_Service_Test.cs
@@ -37,27 +37,5 @@ namespace UserTest
 
             Assert.AreEqual(false, result);
         }
-
-        [TestMethod]
-        public async Task AdministratedTestService_Next_OK()
-        {
-            AdministratedTestService service = new AdministratedTestService();
-            var actualTest = await service.GetAdministratedTestById(1349);
-            var firstQuestion = actualTest.AdministratedQuestions[0];
-            var nextQuestion = await service.Next(actualTest, firstQuestion.Position + 1);
-
-            Assert.AreEqual(actualTest.AdministratedQuestions[1], nextQuestion);
-        }
-
-        [TestMethod]
-        public async Task AdministratedTestService_Previous_OK()
-        {
-            AdministratedTestService service = new AdministratedTestService();
-            var actualTest = await service.GetAdministratedTestById(1349);
-            var secondQuestion = actualTest.AdministratedQuestions[1];
-            var previousQuestion = await service.Previous(actualTest, secondQuestion.Position - 1);
-
-            Assert.AreEqual(actualTest.AdministratedQuestions[0], previousQuestion);
-        }
     }
 }
