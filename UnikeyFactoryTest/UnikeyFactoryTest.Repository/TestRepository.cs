4adda898270d1ecb9bcea8eed8348633918a0c29
CamilloPositano <xcodicsx@gmail.com>
2020-02-24 14:58:48 +0100

    Piccolo fix dei metodi UpdateQuestion nel repository

--- UnikeyFactoryTest/UnikeyFactoryTest.Repository/TestRepository.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Repository/TestRepository.cs
@@ -170,12 +170,5 @@ namespace UnikeyFactoryTest.Repository
 
             return returned;
         }
-        public void UpdateQuestion(QuestionBusiness question)
-        {
-            var newQuestion = QuestionMapper.MapBizToDal(question);
-            var oldQuestion = (EntityExtension) _ctx.Questions.FirstOrDefault(x => x.Id == newQuestion.Id);
-
-            NewUpdate(newQuestion, oldQuestion);
-        }
     }
 }
