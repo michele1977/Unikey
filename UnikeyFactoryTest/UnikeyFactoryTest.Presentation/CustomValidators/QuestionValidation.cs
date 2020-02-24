2c245a65a4157d0ab097e367a26d17005a232652
Abominion <renzolog@hotmail.it>
2020-02-24 16:02:03 +0100

    Modified validation messages style

--- UnikeyFactoryTest/UnikeyFactoryTest.Presentation/CustomValidators/QuestionValidation.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/CustomValidators/QuestionValidation.cs
@@ -13,14 +13,16 @@ namespace UnikeyFactoryTest.Presentation.CustomValidators
         public QuestionValidation()
         {
        
-            RuleFor(question => question.Text).NotEmpty().WithMessage("You must write the question");
+            RuleFor(question => question.Text).NotEmpty().WithMessage("Question's text can't be empty");
             RuleForEach(question => question.Answers).ChildRules(a =>
             {
-                a.RuleFor(answer => answer.Text).NotEmpty().WithMessage("You must write answers");
+                a.RuleFor(answer => answer.Text).NotEmpty().WithMessage("Answer's text can't be empty");
                 a.RuleFor(answer => answer.Score)
                     .NotEmpty()
                     .When(answer => answer.IsCorrectBool, ApplyConditionTo.CurrentValidator)
-                    .WithMessage("Score field is required for correct answer");
+                    .WithMessage("Score field is required for correct answer")
+                    .Must(s => s >= 0)
+                    .WithMessage("Score must be a positive number");
 
             });
             
