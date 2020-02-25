14e2097d01636aa2d1e718429c1f204abdeb698b
Mallqui <hendrichmall99@gmail.com>
2020-02-25 12:00:59 +0100

    Fixati alcuni bug dei form di login e signup

--- UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/TestController.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/TestController.cs
@@ -395,7 +395,7 @@ namespace UnikeyFactoryTest.Presentation.Controllers
 
             testsListModel.Tests = await testsListModel.Paginate(testsListModel.Tests);
 
-            testsListModel.ClosedTestsNumberPerTest = _service.GetClosedTests(testsListModel.PageNumber, testsListModel.PageSize, testsListModel.TextFilter);
+            //testsListModel.ClosedTestsNumberPerTest = _service.GetClosedTests(testsListModel.PageNumber, testsListModel.PageSize, testsListModel.TextFilter);
             
             var testsId = (from t in testsListModel.Tests
                 select t.Id).ToList();
@@ -403,9 +403,9 @@ namespace UnikeyFactoryTest.Presentation.Controllers
 
             testsListModel.ClosedTestsNumberPerTest = await _service.GetClosedTests(testsListModel.PageNumber, testsListModel.PageSize, testsListModel.TextFilter);
 
-            var testsId = (from t in testsListModel.Tests
-                select t.Id).ToList();
-            testsListModel.AdministratedTestOpen = await _service.OpenedTestNumber(testsId);
+            //var testsId = (from t in testsListModel.Tests
+            //    select t.Id).ToList();
+            //testsListModel.AdministratedTestOpen = await _service.OpenedTestNumber(testsId);
 
             return View("TestsList", testsListModel);
         }
--- UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/UserController.cs
+++ UnikeyFactoryTest/UnikeyFactoryTest.Presentation/Controllers/UserController.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Web.WebPages;
+using Microsoft.Ajax.Utilities;
 using UnikeyFactoryTest.Context;
 using UnikeyFactoryTest.IService;
-using UnikeyFactoryTest.Presentation;
 using UnikeyFactoryTest.Presentation.Models;
-using UnikeyFactoryTest.Service;
 
 namespace UnikeyFactoryTest.Presentation.Controllers
 {
@@ -21,22 +15,23 @@ namespace UnikeyFactoryTest.Presentation.Controllers
         {
             service = value;
         }
+
         public ActionResult Index()
         {
             var model = new UserModel();
-            model.UserState = UserState.WaitingFor;
-            
+            model.AreThereMessages = false;
             return View(model);
         }
 
         [HttpPost]
         public async Task<ActionResult> CheckData(UserLoginModel model)
         {
-            var userViewModel = new UserModel();
 
-            if (ModelState.IsValid)
+            var newModel = new UserModel();
+
+            if (ModelState.IsValid == true)
             {
-                var user = new User {Username = model.Username, Password = model.Password};
+                var user = new User { Username = model.Username, Password = model.Password };
 
                 var result = await service.IsUser(user);
 
@@ -49,37 +44,47 @@ namespace UnikeyFactoryTest.Presentation.Controllers
                 }
                 else
                 {
-                    userViewModel.UserState = UserState.IsNotAUser;
-                    return View("Index", userViewModel);
+                    ModelState.Clear();
+                    newModel.AreThereMessages = true;
+                    newModel.ToForm = ToForm.LoginForm;
+                    newModel.LoginModel.UserState = UserState.IsNotAUser;
+                    return View("Index", newModel);
                 }
             }
             else
             {
-                userViewModel.UserState = UserState.WaitingFor;
-                return View("Index", userViewModel);
+                ModelState.Clear();
+                newModel.AreThereMessages = false;
+                newModel.LoginModel.UserState = UserState.WaitingFor;
+                return View("Index", newModel);
             }
+
         }
 
 
         public ActionResult Subscribe(UserSigningUpModel model)
         {
-            var userViewModel = new UserModel();
+            var newModel = new UserModel();
 
             if (ModelState.IsValid == true)
             {
-                User user = new User() {Username = model.Username, Password = model.Password};
+                User user = new User() { Username = model.Username, Password = model.Password };
 
                 service.InsertUser(user);
-                
-                userViewModel.UserState = UserState.RegistrationOk;
-                return View("Index", userViewModel);
+
+                newModel.AreThereMessages = true;
+                newModel.ToForm = ToForm.SigningUpForm;
+                newModel.SigningUpModel.UserState = UserState.RegistrationOk;
+                return View("Index", newModel);
             }
             else
             {
-                userViewModel.UserState = UserState.RegistrationKo;
-                return View("Index", userViewModel);
+                newModel.AreThereMessages = true;
+                newModel.ToForm = ToForm.SigningUpForm;
+                newModel.SigningUpModel.UserState = UserState.RegistrationKo;
+                return View("Index", newModel);
             }
-            
+
         }
 
 
@@ -87,10 +92,33 @@ namespace UnikeyFactoryTest.Presentation.Controllers
         public ActionResult Logout()
         {
             var model = new UserModel();
-            model.UserState = UserState.WaitingFor;
+            model.AreThereMessages = false;
             return View("Index", model);
         }
+
+
+
+        //public ActionResult GetLoginPartial(UserState userState)
+        //{
+        //    var newModel = new UserLoginModel();
+        //    newModel.UserState = userState;
+        //    return PartialView("_LoginFormPartial", newModel);
+        //}
+
+        public ActionResult GetLoginPartial(UserLoginModel model)
+        {
+            var newModel = new UserLoginModel();
+            newModel.UserState = model.UserState;
+            ModelState.Clear();
+            return PartialView("_LoginFormPartial", newModel);
+        }
+
+        public ActionResult GetSignUpPartial(UserSigningUpModel model)
+        {
+            var newModel = new UserSigningUpModel();
+            newModel.UserState = model.UserState;
+            ModelState.Clear();
+            return PartialView("_SigningUpFormPartial", newModel);
+        }
     }
-
-
 }
