09a4ed582414706baaceb23c706769e8e8971cf7
Develollo <lorenzo.caliciotti@gmail.com>
2020-03-17 15:17:47 +0100

    again

--- UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/services/interceptor.service.ts
+++ UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/services/interceptor.service.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {LoginService} from './login.service';
 import {catchError, map, tap} from 'rxjs/operators';
+import {RefreshtokenService} from './refreshtoken.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private service: LoginService) {
+  constructor(private service: LoginService, private refreshService: RefreshtokenService ) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const jwt = localStorage.getItem('token');
     req = req.clone({
-        setHeaders: {
-          Authorization: 'bearer ' + jwt
-        }
+      setHeaders: {
+        Authorization: 'bearer ' + jwt
+      }
       });
 
     return next.handle(req).pipe(
       catchError((error: any) => {
         if (error instanceof HttpErrorResponse && error.status === 401) {
+          if ('Token has expired' === 'Token has expired') {
+              console.log('interceptor');
+              this.refreshService.Refresh();
+              this.service.router.navigateByUrl(req.url).then();
+          }
           this.service.router.navigateByUrl('').then();
         }
         return of(error);
--- UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/services/refreshtoken.service.ts
+++ UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/services/refreshtoken.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {LOCALHOST_URL} from '../constants/api.const';
+import {User} from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RefreshtokeninterceptorService {
+export class RefreshtokenService {
+  LOGIN_URL = LOCALHOST_URL + 'User/Login';
+  user: User;
 
+  constructor(public httpClient: HttpClient) {
+  }
+
+  Refresh() {
+    this.user.username = 'Lollolone';
+    this.user.password = 'Unikey1!';
+    this.httpClient.post<string>(this.LOGIN_URL, this.user).subscribe(
+      jwt => localStorage.setItem('token', jwt));
+    console.log('Refresh');
+  }
 }
