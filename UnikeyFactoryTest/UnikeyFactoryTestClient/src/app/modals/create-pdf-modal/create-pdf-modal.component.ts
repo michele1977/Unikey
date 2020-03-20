30b4670887a72f155cd968f17d80a4e15671a70a
andrea-77 <andrea.guaragna98@gmail.com>
2020-03-20 10:31:26 +0100

    no message

--- UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/modals/create-pdf-modal/create-pdf-modal.component.ts
+++ UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/modals/create-pdf-modal/create-pdf-modal.component.ts
@@ -4,9 +4,6 @@ import {TestService} from '../../services/test.service';
 import { saveAs } from 'file-saver';
 import {Test} from '../../models/test';
 import {LoaderService} from '../../services/loader.service';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-create-pdf-modal',
@@ -15,8 +12,6 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 })
 export class CreatePDFModalComponent implements OnInit {
   @Input() inputTest: Test;
-
-  fileUrl;
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -35,26 +30,4 @@ export class CreatePDFModalComponent implements OnInit {
       this.activeModal.close();
     });
   }
-
-  builderPdf(): string {
-    let builder = '';
-    builder += 'FirstName: _________________________  LastName: _________________________  Date: __ / __ / ____' + '\n\n\n\n';
-    builder += this.inputTest.Title + '\n\n\n\n';
-
-    for (const question of this.inputTest.Questions) {
-      builder += question.Text + '\n\n';
-      for (const answer of question.Answers) {
-        builder += 'O ' + answer.Text + '\n';
-      }
-      builder += '\n';
-    }
-    return builder;
-  }
-
-  openPDF() {
-    const document = { content: this.builderPdf() };
-    pdfMake.createPdf(document).open();
-  }
-
-
 }
