6c6d36b9186af4497cd6be69b8601306ffdc9a54
EasleyNorth <59687487+EasleyNorth@users.noreply.github.com>
2020-04-05 18:33:03 +0200

    Fix ui

---
+++ UnikeyFactoryTest/UnikeyFactoryTestClient/src/app/shared/orderpipe/order.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {OrderPipe} from 'ngx-order-pipe';
+
+@NgModule({
+  declarations: [OrderPipe],
+  exports: [OrderPipe],
+  providers: [OrderPipe]
+})
+export class OrderModule {}
